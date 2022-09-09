409d2057a9b31ff0a3c006c3c7883f4153aae66b
lauren1478 <103980363+lauren1478@users.noreply.github.com>
2022-09-08 20:03:12 -0500

    First Commit

---
+++ QuickDBD-export.sql
@@ -0,0 +1,26 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Crime_Reports" (
+    "Zip_Code" int   NOT NULL,
+    "Occured_Date" date   NOT NULL,
+    "APD_District" var   NOT NULL,
+    "Highest_Offense_Description" var   NOT NULL
+);
+
+CREATE TABLE "Austin_Housing_Market" (
+    "Zip_Code" int   NOT NULL,
+    "Population_Below_Poverty_Level" int   NOT NULL,
+    "Median_Household_Income" int   NOT NULL,
+    "Unemployment" int   NOT NULL,
+    "Median_Home_Value" int   NOT NULL,
+    "Change_Number_of_Housing_Units" int   NOT NULL,
+    "Change_in_Median_Rent" int   NOT NULL,
+    "Rentals_less_than_25K_income" int   NOT NULL,
+    "Homes_less_than_50k_income" int   NOT NULL
+);
+
+ALTER TABLE "Crime_Reports" ADD CONSTRAINT "fk_Crime_Reports_Zip_Code" FOREIGN KEY("Zip_Code")
+REFERENCES "Austin_Housing_Market" ("Zip_Code");
+
